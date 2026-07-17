256cc25be8e6c1cf6953b107b9c5655ea71ae37b
NNguyenK18 <nhatnguyen211278@gmail.com>
2026-07-17 11:12:32 +0700

    debug: print OIDC sub claim to diagnose AssumeRoleWithWebIdentity failure

--- terraform/ecr/main.tf
+++ terraform/ecr/main.tf
@@ -35,4 +35,8 @@ resource "aws_ecr_lifecycle_policy" "app" {
 resource "aws_iam_openid_connect_provider" "github" {
   url            = "https://token.actions.githubusercontent.com"
   client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [
+    "ab9d0263244dd0326eb67015705a667e79cfe998",
+    "227203b5317f3818cab5b5ce596132bf36748c0e",
+  ]
 }
